bd15c3b7e9f88ec511ae0b71505eee8e203f6e12
thomasmitchum <thomas.d.mitchum@gmail.com>
2022-01-10 20:48:40 -0500

    Added Dependencies

---
+++ main.py
@@ -0,0 +1,12 @@
+import requests
+from colorama import init, Fore, Back
+
+init()
+
+response = requests.get(
+    'https://icanhazdadjoke.com/',
+    headers={'Accept':'application/json'})
+
+print( Back.BLUE + Fore.YELLOW +'Your dad joke: {0}'.format( response.json()['joke']))
+
+input("Press ENTER to continue!")
